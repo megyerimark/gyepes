35b42c83a808013b2e4c0735d5a640ad192c30e4
Megyeri Márk <megyko1234@gmail.com>
2022-11-09 09:00:52 +0100

    Képlet hozzáadas javítva version 0.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gyepes';
   diameter !:number;
+  price !:number;
 
 
   calArea() :void{
-    let radius = this.diameter/2;
-    let area = Math.pow(radius , 2)*Math.PI;
-    alert ("Működik")
+    let radius = this.diameter / 2;
+    let area = Math.pow(radius , 2) * Math.PI;
+    let halfArea = area / 2;
+    this.price = halfArea * 2500;
+    // alert ("Működik")
   }
 }
